c07680de6c40168607043d377c475fa7df50cb37
Aviv Komemi <komemiav@gmail.com>
2019-09-10 13:28:36 +0300

    Updated Scenes
    * TouchInputScene
    - Updated appearance to more closely resemble original concept.
    - Detects dragging gesture to top of screen.
    
    * TouchInput.cs
    - Slight improvements to reliability.

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -4,25 +4,25 @@ using UnityEngine.UI;
 
 public class TouchInput : MonoBehaviour
 {
-    public float maxTimeBetweenTaps = 0.2f;
-    public LayerMask layerMask;
+    [SerializeField] float maxTimeBetweenTaps = 0.2f;
+    [SerializeField] LayerMask layerMask;
 
-    [SerializeField]float timeCounter;
-    [SerializeField] float tapTime;
-    [SerializeField] int tapCount;
-    [SerializeField] Vector3 rectStartPos;
-    [SerializeField] Vector3 rectDeltaPos;
-    [SerializeField] Vector3 touchStartPos;
-    [SerializeField] Camera mainCam;
-    [SerializeField] Touch touch;
-    [SerializeField] RectTransform rectTransform;
+    float timeCounter;
+    float tapTime;
+    int tapCount;
+    Vector3 rectStartPos;
+    Vector3 rectDeltaPos;
+    Vector3 touchStartPos;
+    Rect foldActionArea;
+    Touch touch;
+    Camera mainCam;
+    RectTransform rectTransform;
 
     private void Start()
     {
-        //rectTransform = GetComponent<RectTransform>();
         mainCam = Camera.main;
-        //startPos = rectTransform.position;
         rectDeltaPos = new Vector3(0, 0, mainCam.nearClipPlane);
+        foldActionArea = new Rect(Screen.width / 2, Screen.height * 4 / 5, Screen.width * 6 / 8, Screen.height / 5);
     }
     private void Update()
     {
@@ -67,33 +67,33 @@ public class TouchInput : MonoBehaviour
                 tapCount = 0;
                 timeCounter = 0;
             }
-                
+
         }
     }
     bool ClickedDraggable()
     {
-        RaycastHit2D hit= Physics2D.Raycast(touch.position, Vector2.zero, mainCam.farClipPlane, layerMask);
+        RaycastHit2D hit = Physics2D.Raycast(touch.position, Vector2.zero, mainCam.farClipPlane, layerMask);
         //Debug.Log("Touched at " + touch.position);
         if (hit.collider != null)
         {
             Debug.Log("Clicked " + hit.collider.name);
             rectTransform = hit.collider.GetComponent<RectTransform>();
             rectStartPos = rectTransform.position;
-            touchStartPos = touch.position;
             return true;
         }
         return false;
     }
     void ReleaseHeldDraggable()
     {
-        Debug.Log("Released object");
+        if (foldActionArea.Contains(Input.GetTouch(0).position))
+            Debug.Log("Folded");
         rectTransform.position = rectStartPos;
         rectTransform = null;
     }
     void DragHeldObject()
     {
-        rectDeltaPos.x = Input.GetTouch(0).position.x - touchStartPos.x;
-        rectDeltaPos.y = Input.GetTouch(0).position.y - touchStartPos.y;
+        rectDeltaPos.x = Input.GetTouch(0).position.x - rectStartPos.x;
+        rectDeltaPos.y = Input.GetTouch(0).position.y - rectStartPos.y;
         rectTransform.position = rectStartPos + rectDeltaPos;
     }
 }
